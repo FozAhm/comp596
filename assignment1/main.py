01e056049b3a724c5db2f1a949f95fdbdc211315
Fozail Ahmad <fozail.ahmad@broadsign.com>
2019-09-24 13:07:00 -0400

    Added Degree Dist

--- assignment1/main.py
+++ assignment1/main.py
@@ -2,7 +2,32 @@ from scipy.sparse import csc_matrix
 import sys
 import string
 
-def import_network(filename):
+def str_to_bool(s):
+    if s == 'True':
+         return True
+    elif s == 'true':
+        return True
+    elif s == 'False':
+         return False
+    elif s == 'false':
+        return False
+    else:
+         raise ValueError("Cannot covert {} to a bool".format(s))
+
+def list_frequency(list):
+
+    frequency_dict = {}
+
+    for degree in list:
+
+        if frequency_dict.get(degree):
+            frequency_dict[degree] += 1
+        else:
+            frequency_dict[degree] = 1
+    
+    return frequency_dict
+
+def import_network(filename, undirected):
     file = open(filename, 'r')
 
     row = []
@@ -14,6 +39,11 @@ def import_network(filename):
         row.append(int(vertex[0]))
         col.append(int(vertex[1]))
         data.append(1)
+
+        if (undirected and (vertex[1] != vertex[0])):
+            row.append(int(vertex[1]))
+            col.append(int(vertex[0]))
+            data.append(1)
     
     file.close()
 
@@ -21,7 +51,7 @@ def import_network(filename):
 
     return csc_matrix((data, (row, col)), shape=(number_of_nodes, number_of_nodes))
 
-def degree_distribution(matrix, size):
+def matrix_degrees(matrix, size):
 
     degrees = []
 
@@ -29,26 +59,44 @@ def degree_distribution(matrix, size):
 
         col = matrix.getcol(i)
         degrees.append(col.sum())
-        print('Col', i, ':', col.sum(), '\n', col.toarray())
+        #print('Col', i, ':', col.sum(), '\n', col.toarray())
 
     return degrees
 
+def degree_distribution(degree_frequency, size):
+
+    degree = []
+    porbability = []
+
+    for key, value in degree_frequency.items():
+        degree.append(key)
+        porbability.append(value/size)
+    
+    return [degree,porbability]
 
 
 
 network_file = sys.argv[1]
+undirected = str_to_bool(sys.argv[2])
 
-sparse_matrix = import_network(network_file)
+sparse_matrix = import_network(network_file, undirected)
 
 nodes = sparse_matrix.get_shape()[1]
 
 print('Number of Nodes:', nodes)
-print('Matrix: \n', sparse_matrix.toarray())
+#print('Matrix: \n', sparse_matrix.toarray())
 
-degrees = degree_distribution(sparse_matrix, nodes)
+degrees = matrix_degrees(sparse_matrix, nodes)
 
-print(degrees)
+#print(degrees)
 
+degree_frequency = list_frequency(degrees)
+
+#print(degree_frequency)
+
+degree_dist = degree_distribution(degree_frequency, nodes)
+
+print(degree_dist[1])
 
 
 
