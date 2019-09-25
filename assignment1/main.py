6d3770c59edd28ec32b7ec866567c359996cf1de
Fozail Ahmad <fozail.ahmad@broadsign.com>
2019-09-24 23:11:55 -0400

    Added Option B Fully

--- assignment1/main.py
+++ assignment1/main.py
@@ -73,33 +73,37 @@ def matrix_degrees(matrix, size, undirected=True):
 
     return degrees
 
-def degree_distribution(degree_frequency, size):
+def probability_distribution(frequency, size):
 
     degree = []
     porbability = []
 
-    for key, value in degree_frequency.items():
+    for key, value in frequency.items():
         degree.append(key)
         porbability.append(value/size)
     
     return [degree,porbability]
 
-def print_scatter_plot(scatter_plot, xaxis='Degree', yaxis='Probability of Degree', title='Degree Destribution:',log=True):
+def print_scatter_plot(scatter_plot, xaxis='Degree', yaxis='Probability of Degree', title='Degree Destribution:',log=True, logx=True, tofit=True, message=''):
 
-    if log:
-        fit = np.polyfit(np.log10(scatter_plot[0]), np.log10(scatter_plot[1]), 1)
+    if tofit:
+        if log:
+            fit = np.polyfit(np.log10(scatter_plot[0]), np.log10(scatter_plot[1]), 1)
+        else:
+            fit = np.polyfit(scatter_plot[0], scatter_plot[1], 1)
+        
+        best_fit = '\ny = ' + str(np.round(fit[0], 6)) + 'x + ' + str(np.round(fit[1], 6)) 
     else:
-        fit = np.polyfit(scatter_plot[0], scatter_plot[1], 1)
+        best_fit = ''
 
-    best_fit = 'y = ' + str(np.round(fit[0], 6)) + 'x + ' + str(np.round(fit[1], 6)) 
-    
     plt.plot(scatter_plot[0], scatter_plot[1], 'bo')
     
     if log:
         plt.yscale('log')
+    if logx:
         plt.xscale('log')
 
-    plt.title(title + '\n' + best_fit)
+    plt.title(title + best_fit + message)
     plt.xlabel(xaxis)
     plt.ylabel(yaxis)
     
@@ -107,6 +111,29 @@ def print_scatter_plot(scatter_plot, xaxis='Degree', yaxis='Probability of Degre
     
     plt.show()
     plt.close()
+
+def calculate_clustering(A3_diagonal, degrees, size):
+
+    clustering = []
+
+    for i in range(size):
+
+        degree = degrees[i]
+
+        if (A3_diagonal[i] == 0):
+            clustering.append(0)
+        else:
+            clustering.append(A3_diagonal[i]/(degree*(degree-1)))
+    
+    return clustering
+
+def list_average(lst):
+    
+    list_sum = sum(lst)
+    list_length = len(lst)
+    list_average = list_sum/list_length
+
+    return list_average
 
 network_file = sys.argv[1]
 undirected = str_to_bool(sys.argv[2])
@@ -117,24 +144,38 @@ sparse_matrix = import_network(network_file, undirected)
 print('Done Loading')
 #print('Matrix: \n', sparse_matrix.toarray())
 
-nodes = sparse_matrix.get_shape()[1]
-print('Number of Nodes in Graph:', nodes)
+number_of_nodes = sparse_matrix.get_shape()[1]
+print('Number of Nodes in Graph:', number_of_nodes)
+
+degrees = matrix_degrees(sparse_matrix, number_of_nodes, undirected)
+#print('Degrees:\n', degrees)
 
 if(option == 'a'):
-    
-    degrees = matrix_degrees(sparse_matrix, nodes, undirected)
-    #print('Degrees:\n', degrees)
 
     degree_frequency = list_frequency(degrees)
-    #print(degree_frequency)
+    #print('Degree Frequency: ', degree_frequency)
 
-    degree_dist = degree_distribution(degree_frequency, nodes)
+    degree_dist = probability_distribution(degree_frequency, number_of_nodes)
     print_scatter_plot(degree_dist)
 elif(option == 'b'):
     
     A3 = sparse_matrix*sparse_matrix*sparse_matrix
-    #print(A3.toarray())
+    #print('A3 Matrix: \n', A3.toarray())
+
     A3_diagonal = A3.diagonal()
+    #print('A3 Diaganol: ', A3_diagonal)
+
+    clustering_list = calculate_clustering(A3_diagonal, degrees, number_of_nodes)
+    #print('Clustering: \n',clustering_list)
+
+    clustering_average = list_average(clustering_list)
+    #print('Clustering Coeffecient Average: ', clustering_average)
+
+    clustering_frequency = list_frequency(clustering_list)
+    #print('Clustering Frequency: \n', clustering_frequency)
+
+    clustering_dist = probability_distribution(clustering_frequency, number_of_nodes)
+    print_scatter_plot(clustering_dist, xaxis='Clustering Coeffecient', yaxis='Probability of Clustering Coeffecient', title='Clustering Coeffecient Distribution', log=True, logx=False, tofit=False, message='\nClustering Coeffecient Average: ' + str(np.round(clustering_average, 6)))
     
 else:
     print('Incorrect Option Selected...')
