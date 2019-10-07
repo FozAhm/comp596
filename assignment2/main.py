360f2f5198f83ad9cfdf81da6395e3b83b1c5aea
Fozail Ahmad <fozail.ahmad@broadsign.com>
2019-10-06 22:21:36 -0400

    Added Reading GCN Data Ability

--- assignment2/main.py
+++ assignment2/main.py
@@ -1,8 +1,38 @@
 import sys
 import networkx as nx
+import pickle as pkl
 
-file_name = sys.argv[1]
+def str_to_bool(s):
+    if s == 'True':
+         return True
+    elif s == 'true':
+        return True
+    elif s == 'False':
+         return False
+    elif s == 'false':
+        return False
+    else:
+         raise ValueError("Cannot covert {} to a bool".format(s))
 
-G = nx.read_gml(file_name, label='id')
+option = sys.argv[1]
+file_name = sys.argv[2]
 
-print('Test')
+if option == 'a':
+    print('Real Data')
+    G = nx.read_gml(file_name, label='id')
+elif option == 'b':
+    print('GNC Data')
+
+    data = []
+    with open(file_name + '.ally', 'rb') as f:
+        data.append(pkl.load(f, encoding='latin1'))
+    with open(file_name + '.graph', 'rb') as f:
+        data.append(pkl.load(f, encoding='latin1'))
+    
+    print('All y data: ', data[0])
+    print('Graph: ', data[1])
+
+elif option == 'c':
+    print('Synthetic Data')
+else:
+    print('Wrong Option Selected')
