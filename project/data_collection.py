8f6e8ea7a0dbcd49a2a90954d612e96204177a90
Fozail Ahmad <25251550+FozAhm@users.noreply.github.com>
2019-11-17 14:56:57 -0500

    Improved Data Colleciton Script

--- project/data_collection.py
+++ project/data_collection.py
@@ -1,17 +1,21 @@
 import sys
 import urllib.request
+import os.path
 
 print('Data Collection Starting')
 
 month = sys.argv[1]
+begin_day = int(sys.argv[2]) # Shoud start at 1
+end_day = int(sys.argv[3]) # Should finish at 32
 
 url = 'https://data.gharchive.org/2018-01-01-0.json.gz'
 store = '/Users/fozail/SchoolDev/comp596/project/data/2018-01-01-0.json.gz'
 
 opener = urllib.request.URLopener()
+# Add a header to throw off crawler protection on wesite
 opener.addheader('User-Agent', 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3')
 
-for day in range(1, 32):
+for day in range(begin_day, end_day):
     for hour in range(0, 24):
         if day < 10:
             url = 'https://data.gharchive.org/2018-' + month + '-0' + str(day) + '-' + str(hour) + '.json.gz'
@@ -19,9 +23,10 @@ for day in range(1, 32):
         else:
             url = 'https://data.gharchive.org/2018-' + month + '-' + str(day) + '-' + str(hour) + '.json.gz'
             store = '/Users/fozail/SchoolDev/comp596/project/data/2018-' + month + '-' + str(day) + '-' + str(hour) + '.json.gz'
-
-        try:
-            opener.retrieve(url, store)
-        except Exception as e:
-            print('Could not retreive data for URL:', url)
-            print('Reason:', e)
+        
+        if os.path.isfile(store) == False:
+            try:
+                opener.retrieve(url, store)
+            except Exception as e:
+                print('Could not retreive data for URL:', url)
+                print('Reason:', e)
