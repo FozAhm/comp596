3c29a2b38113118ad54be95c08e4d0975c216776
Fozail Ahmad <25251550+FozAhm@users.noreply.github.com>
2019-11-28 10:48:25 -0500

    Added Data Aggregation Sript

---
+++ project/data_aggregation.py
@@ -0,0 +1,34 @@
+import sys
+import time
+import csv
+import os
+
+start_time = time.time()
+
+print('Data Aggregation Script')
+
+event_type = sys.argv[1] # Should be Edges or Nodes for aggregation
+data_location = sys.argv[2] # Should be '/Users/fozail/SchoolDev/comp596/project/data/'
+output_location = sys.argv[3] # Should be '/Users/fozail/SchoolDev/comp596/project/edges/'
+
+print('Looking for', event_type)
+
+output_file = open(output_location, 'a')
+output_writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+data_directory = os.fsencode(data_location)
+
+all_nodes = {}
+
+for file in os.listdir(data_directory):
+    filename = os.fsdecode(file)
+    file_path = data_location + filename
+    
+    with open(file_path) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+
+        for row in csv_reader:
+            all_nodes[row[0]] = row[1]
+
+for key,value in all_nodes.items():
+    output_writer.writerow([key, value])
--- project/testing_playground.py
+++ project/testing_playground.py
@@ -8,3 +8,19 @@ print(test_set)
 
 test_set.add('apple')
 print(test_set)
+
+test_dict = {
+    'linux' : 'repo',
+    'ruby' : 'repo',
+    'fozail' : 'user'
+}
+print(test_dict)
+
+test_dict['sun'] = 'user'
+print(test_dict)
+
+test_dict['fozail'] = 'repo'
+print(test_dict)
+
+for key,value in test_dict.items():
+    print('Key:', key, 'Value:', value)
