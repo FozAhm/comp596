f36b1f653044b906bf74f5bb9bae32fe08a7011f
Fozail Ahmad <25251550+FozAhm@users.noreply.github.com>
2019-12-05 01:24:30 -0500

    Added New Graph Analysis Script

---
+++ project/graph_analysis.py
@@ -0,0 +1,16 @@
+import networkx as nx
+import sys
+import csv
+import time
+
+start_time = time.time()
+
+print('Graph Analysis...')
+
+data_location = sys.argv[1]
+
+G = nx.read_graphml(data_location)
+print('Number of Nodes:', len(G))
+print('Number of Edges:', G.size())
+
+print("--- %s seconds ---" % (time.time() - start_time))
