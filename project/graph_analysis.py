abcb512c573e6ec66398cf993eba6126be11fba3
Fozail Ahmad <25251550+FozAhm@users.noreply.github.com>
2019-12-05 01:30:49 -0500

    Added Time tracking

--- project/graph_analysis.py
+++ project/graph_analysis.py
@@ -10,7 +10,10 @@ print('Graph Analysis...')
 data_location = sys.argv[1]
 
 G = nx.read_gml(data_location)
+
+print("--- Graph Load Time ---\n--- %s seconds ---" % (time.time() - start_time))
+
 print('Number of Nodes:', len(G))
 print('Number of Edges:', G.size())
 
-print("--- %s seconds ---" % (time.time() - start_time))
+print("--- Total Execution Time ---\n--- %s seconds ---" % (time.time() - start_time))
