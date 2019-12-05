48f48a48fab9d1412b9e183b2edac8bb8aff4f10
Fozail Ahmad <25251550+FozAhm@users.noreply.github.com>
2019-12-05 17:15:08 -0500

    Added Automation

--- project/update_script2.sh
+++ project/update_script2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-git clone https://github.com/FozAhm/comp596.git /home/ubuntu/comp596
-chmod +x /home/ubuntu/comp596/project/update_script.sh
-./home/ubuntu/comp596/project/update_script.sh
+/bin/su -c "git clone https://github.com/FozAhm/comp596.git /home/ubuntu/comp596" - ubutnu
+/bin/su -c "chmod +x /home/ubuntu/comp596/project/update_script.sh" - ubuntu
+/bin/su -c "/home/ubuntu/comp596/project/update_script.sh" - ubuntu
