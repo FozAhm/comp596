8ae18ff6f92ead570295f3fb82607ad6ad5a8b47
Fozail Ahmad <25251550+FozAhm@users.noreply.github.com>
2019-12-05 19:17:03 -0500

    Added User Interface =

--- project/graph_analysis.py
+++ project/graph_analysis.py
@@ -11,7 +11,6 @@ print('Graph Analysis...')
 
 data_location = sys.argv[1]
 read_format = sys.argv[2]
-option = sys.argv[3]
 
 if read_format == 'gml':
     G = nx.read_gml(data_location)
@@ -20,27 +19,39 @@ elif read_format == 'gpkl':
 
 print("--- Graph Load Time ---\n--- %s seconds ---" % (time.time() - start_time))
 
-if option == 'numnodes':
-    print('Number of Nodes:', len(G))
-elif option == 'numedges':
-    print('Number of Edges:', G.size())
-elif option == 'degreehist':
-    #degree_list = nx.degree_histogram(G)
-    #print(degree_list)
+run = True
+while(run):
+    option = input("Enter your option : ")
 
-    degree_sequence = sorted([d for n, d in G.degree()], reverse=True)  # degree sequence
-    # print "Degree sequence", degree_sequence
-    degreeCount = collections.Counter(degree_sequence)
-    deg, cnt = zip(*degreeCount.items())
+    option_start_time = time.time()
 
-    fig, ax = plt.subplots()
-    plt.bar(deg, cnt, width=0.80, color='b')
+    if option == 'numnodes':
+        print('Number of Nodes:', len(G))
+    elif option == 'numedges':
+        print('Number of Edges:', G.size())
+    elif option == 'degreehist':
+        #degree_list = nx.degree_histogram(G)
+        #print(degree_list)
 
-    plt.title("Degree Histogram")
-    plt.ylabel("Count")
-    plt.xlabel("Degree")
-    ax.set_xticks([d + 0.4 for d in deg])
-    ax.set_xticklabels(deg)
-    plt.savefig('degree_dist2.png')
+        degree_sequence = sorted([d for n, d in G.degree()], reverse=True)  # degree sequence
+        # print "Degree sequence", degree_sequence
+        degreeCount = collections.Counter(degree_sequence)
+        deg, cnt = zip(*degreeCount.items())
+
+        fig, ax = plt.subplots()
+        plt.bar(deg, cnt, width=0.80, color='b')
+
+        plt.title("Degree Histogram")
+        plt.ylabel("Count")
+        plt.xlabel("Degree")
+        ax.set_xticks([d + 0.4 for d in deg])
+        ax.set_xticklabels(deg)
+        plt.savefig('degree_dist2.png')
+    elif option == 'break':
+        run = False
+    else:
+        print('Wrong Option')
+    
+    print("--- Total Option Execution Time ---\n--- %s seconds ---" % (time.time() - option_start_time))
 
 print("--- Total Execution Time ---\n--- %s seconds ---" % (time.time() - start_time))
