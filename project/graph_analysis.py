586e5119033e62c3c2615929a2bb03ea4b8fbba4
Fozail Ahmad <25251550+FozAhm@users.noreply.github.com>
2019-12-05 19:36:04 -0500

    Removed User Interface, fixed plt bug

--- project/graph_analysis.py
+++ project/graph_analysis.py
@@ -12,6 +12,7 @@ print('Graph Analysis...')
 
 data_location = sys.argv[1]
 read_format = sys.argv[2]
+option = sys.argv[3]
 
 if read_format == 'gml':
     G = nx.read_gml(data_location)
@@ -20,29 +21,23 @@ elif read_format == 'gpkl':
 
 print("--- Graph Load Time ---\n--- %s seconds ---" % (time.time() - start_time))
 
-run = True
-while(run):
-    option = input("Enter your option : ")
+option_start_time = time.time()
 
-    option_start_time = time.time()
+if option == 'numnodes':
+    print('Number of Nodes:', len(G))
+elif option == 'numedges':
+    print('Number of Edges:', G.size())
+elif option == 'degreehist':
+    degree_list = nx.degree_histogram(G)
+    #print(degree_list)
 
-    if option == 'numnodes':
-        print('Number of Nodes:', len(G))
-    elif option == 'numedges':
-        print('Number of Edges:', G.size())
-    elif option == 'degreehist':
-        degree_list = nx.degree_histogram(G)
-        #print(degree_list)
+    x = np.arange(1, len(degree_list)+1)
 
-        x = np.arange(1, len(degree_list))
+    plt.plot(x, degree_list, 'bo')
+    plt.savefig('degree_dist.png')
+else:
+    print('Wrong Option')
 
-        plt.plot(x, degree_list, 'bo')
-        plt.savefig('degree_dist.png')
-    elif option == 'break':
-        run = False
-    else:
-        print('Wrong Option')
-    
-    print("--- Total Option Execution Time ---\n--- %s seconds ---" % (time.time() - option_start_time))
+print("--- Total Option Execution Time ---\n--- %s seconds ---" % (time.time() - option_start_time))
 
-print("--- Total Execution Time ---\n--- %s seconds ---" % (time.time() - start_time))
+print("--- Total Program Execution Time ---\n--- %s seconds ---" % (time.time() - start_time))
